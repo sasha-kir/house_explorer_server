5aae0d6049089e77bb00ce94836a2b6373a2041d
Sasha Kirillov <sasha.anaximander@gmail.com>
2019-10-06 18:33:19 +0300

    added database connection and /register route

---
+++ models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
+
+from server import db, bcrypt
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    joined_at = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+class Auth(db.Model):
+    __tablename__ = 'auth'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    _password = db.Column('password', db.Binary(60), nullable=False)
+
+    def __init__(self, email, plaintext_password):
+        self.email = email
+        self.password = plaintext_password
+ 
+    @hybrid_property
+    def password(self):
+        return self._password
+ 
+    @password.setter
+    def password(self, plaintext_password):
+        self._password = bcrypt.generate_password_hash(plaintext_password)
+ 
+    @hybrid_method
+    def is_correct_password(self, plaintext_password):
+        return bcrypt.check_password_hash(self.password, plaintext_password)
--- server.py
+++ server.py
@@ -1,16 +1,67 @@
 import os
+from termcolor import colored
 
-from flask import Flask
+from flask import Flask, Response, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy_utils import database_exists, create_database, drop_database
+from sqlalchemy.exc import IntegrityError
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
+DB_URL = os.environ['DATABASE_URL']
+
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+bcrypt = Bcrypt(app)
+BCRYPT_LOG_ROUNDS = 15
+
+db = SQLAlchemy(app)
+
+from models import User, Auth
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
 
-@app.route('/<name>')
-def hello_name(name):
-    return "Hello {}!".format(name)
+@app.route('/register', methods=['POST'])
+def register_user():
+    username = request.json['username']
+    email = request.json['email']
+    password = request.json['password']
+
+    new_user = User(username=username, email=email)
+    new_auth = Auth(email=email, plaintext_password=password)
+
+    db.session.add(new_user)
+    db.session.add(new_auth)
+
+    try:
+        db.session.commit()
+        return jsonify({ 'username': username, 'email': email }), 200
+    except IntegrityError:
+        db.session.rollback()
+        return jsonify({ 'error': 'email already exists' }), 400
+
+
+
+@app.cli.command('resetdb')
+def resetdb_command():
+    """
+    Destroys and creates the database + tables.
+    from https://vsupalov.com/flask-sqlalchemy-postgres/
+    """
+
+    if database_exists(DB_URL):
+        print('Deleting database.')
+        drop_database(DB_URL)
+    if not database_exists(DB_URL):
+        print('Creating database.')
+        create_database(DB_URL)
+
+    print('Creating tables.')
+    db.create_all()
+    print(colored('Database ready!', 'green'))
