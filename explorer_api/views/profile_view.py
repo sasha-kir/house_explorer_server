afe496e686222347f1337dac148d3ff1b249a030
Sasha Kirillov <sasha.anaximander@gmail.com>
2019-10-21 16:27:06 +0300

    added userpics from gravatar to profile endpoint

--- explorer_api/views/profile_view.py
+++ explorer_api/views/profile_view.py
@@ -1,14 +1,13 @@
 from datetime import datetime
-
+from hashlib import md5
 from flask import request, jsonify, Blueprint
-from sqlalchemy.exc import IntegrityError
 
-from ..models import db
 from ..models.User import User
 
 from ..shared.Authentication import TokenAuth
 
 profile = Blueprint('profile', __name__)
+
 
 @profile.route('/profile', methods=['POST'])
 def get_user_profile():
@@ -27,8 +26,29 @@ def get_user_profile():
     current_date = datetime.now().date()
     dates_delta = current_date - user_entry.joined_at.date()
 
+    email_hash = md5(bytes(email, encoding="utf-8")).hexdigest()
+    gravatar_link = 'https://www.gravatar.com/avatar/' + email_hash + "?d=mp&s=200"
+
     return jsonify({ 
+        "userPic": gravatar_link,
         "username": user_entry.username,
         "email": email,
         "daysRegistered": dates_delta.days,
+    }), 200
+
+
+@profile.route('/userpic', methods=['POST'])
+def get_userpic():
+    token = request.json["token"]
+    decode_result = TokenAuth.decode_token(token)
+    email = decode_result.get("user_email", "")
+
+    if not email:
+        return jsonify({ "error": "invalid token" }), 400
+
+    email_hash = md5(bytes(email, encoding="utf-8")).hexdigest()
+    gravatar_link = 'https://www.gravatar.com/avatar/' + email_hash + "?d=mp&s=100"
+
+    return jsonify({
+        "userPic": gravatar_link
     }), 200
