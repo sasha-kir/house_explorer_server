81dbae51baef0daaf34f225bb71ffa4732dab192
Sasha Kirillov <sasha.anaximander@gmail.com>
2019-11-02 23:42:23 +0300

    fixed date field in history table

--- explorer_api/models/History.py
+++ explorer_api/models/History.py
@@ -7,7 +7,7 @@ class History(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, nullable=False)
-    added_at = db.Column(db.DateTime, nullable=False, default=datetime.now().date)
+    added_at = db.Column(db.DateTime, nullable=False)
     house_info = db.Column(db.JSON, nullable=False)
     house_coords = db.Column(db.String(50), nullable=False)
 
@@ -15,6 +15,7 @@ class History(db.Model):
         self.user_id = user_id
         self.house_info = house_info
         self.house_coords = house_coords
+        self.added_at = datetime.now().date()
 
     def __repr__(self):
         return '<History entry by user %r at %r>' % (self.user_id, self.added_at)
